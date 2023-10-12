bf6a0a16fba41f4954b3b3cb1b7dbbc1ecf318ab
mikhail <kuzmish02@gmail.com>
2023-10-13 01:01:26 +0300

    runge 4 не работает

--- zephyr/math/solver/sm_fluid.cpp
+++ zephyr/math/solver/sm_fluid.cpp
@@ -77,158 +77,165 @@ void SmFluid::update(EuMesh& mesh, IdealGas &eos) {
 
     for (auto cell: mesh) {
         // Примитивный вектор в ячейке
-        PState zc = cell(U).get_state1();
-
+        PState zc = cell(U).get_state(0);
         // Консервативный вектор в ячейке
         QState qc(zc);
-
-        // Переменная для потока
-        Flux flux;
-        for (auto& face: cell.faces()) {
-            // Внешняя нормаль
-            auto &normal = face.normal();
-
-            // Примитивный вектор соседа
-            PState zn(zc);
-
-            //Единмтаенное различие
-            if (face.is_boundary()) {
-                // Граничные условия типа "стенка"
-                double vn = zc.velocity.dot(face.normal());
-                zn.velocity -= 2.0 * vn * face.normal();
-            } 
-            else {
-                zn = face.neib()(U).get_state1();
-            }
-
-            // Значение на грани со стороны ячейки
-            PState zm = zc.in_local(normal);
-
-            // Значение на грани со стороны соседа
-            PState zp = zn.in_local(normal);
-
-            // Численный поток на грани
-            auto loc_flux = m_nf->flux(zm, zp, m_eos);
-            loc_flux.to_global(normal);
-
-            // Суммируем поток
-            flux.vec() += loc_flux.vec() * face.area();
-        }
-
+        // Расчет потока P1
+        Flux P1 = calc_flux(cell, 0);
         // Новое значение в ячейке (консервативные переменные)
-        QState Qc = qc.vec() - m_dt * flux.vec() / cell.volume();
-
+        QState Qc = qc.vec() - m_dt * P1.vec() / cell.volume();
         // Новое значение примитивных переменных
         PState Zc(Qc, m_eos);
-
-        cell(U).set_state2(Zc);
+        //
+        cell(U).set_state(Zc,2);
     }
 };
 
 // Рунге-Кутта 2ого порядка
 void SmFluid::fluxes2(Mesh &mesh) {
-    for (auto cell: mesh) {
+    for (auto cell : mesh) {
         // Примитивный вектор в ячейке
-        PState zc = cell(U).get_state1();
-
+        PState zc = cell(U).get_state(0);
         // Консервативный вектор в ячейке
         QState qc(zc);
-
-        // Переменная для потока
-        Flux flux;
-        for (auto& face: cell.faces()) {
-            // Внешняя нормаль
-            auto &normal = face.normal();
-
-            // Примитивный вектор соседа
-            PState zn(zc);
-
-            if (face.is_boundary()) {
-                // Граничные условия типа "стенка"
-                double vn = zc.velocity.dot(face.normal());
-                zn.velocity -= 2.0 * vn * face.normal();
-            }
-            else {
-                zn = face.neib()(U).get_state1();
-            }
-
-            // Значение на грани со стороны ячейки
-            PState zm = zc.in_local(normal);
-
-            // Значение на грани со стороны соседа
-            PState zp = zn.in_local(normal);
-
-            // Численный поток на грани
-            auto loc_flux = m_nf->flux(zm, zp, m_eos);
-            loc_flux.to_global(normal);
-
-            // Суммируем поток
-            flux.vec() += loc_flux.vec() * face.area();
-        }
-
-        // Новое значение в ячейке (консервативные переменные)
-        QState Qc = qc.vec() - 0.5 * m_dt * flux.vec() / cell.volume();
-
-        // Новое значение примитивных переменных
-        PState Zc(Qc, m_eos);
-
-        cell(U).set_state_h(Zc);
+        // Расчет потока P1
+        Flux P1 = calc_flux(cell, 0);
+        //
+        QState Qch = qc.vec() - 0.5 * m_dt / cell.volume() * P1.vec();
+        //
+        PState Zch(Qch, m_eos);
+        //
+        cell(U).set_state(Zch, 1);
     }
-
-    for (auto cell: mesh) {
+    for (auto cell : mesh) {
         // Примитивный вектор в ячейке
-        PState zc = cell(U).get_state_h();
-
+        PState zc = cell(U).get_state(0);
         // Консервативный вектор в ячейке
         QState qc(zc);
-
-        // Переменная для потока
-        Flux flux;
-        for (auto& face: cell.faces()) {
-            // Внешняя нормаль
-            auto &normal = face.normal();
-
-            // Примитивный вектор соседа
-            PState zn(zc);
-
-            if (face.is_boundary()) {
-                // Граничные условия типа "стенка"
-                double vn = zc.velocity.dot(face.normal());
-                zn.velocity -= 2.0 * vn * face.normal();
-            }
-            else {
-                zn = face.neib()(U).get_state_h();
-            }
-
-            // Значение на грани со стороны ячейки
-            PState zm = zc.in_local(normal);
-
-            // Значение на грани со стороны соседа
-            PState zp = zn.in_local(normal);
-
-            // Численный поток на грани
-            auto loc_flux = m_nf->flux(zm, zp, m_eos);
-            loc_flux.to_global(normal);
-
-            // Суммируем поток
-            flux.vec() += loc_flux.vec() * face.area();
-        }
-
-        // Примитивный вектор в ячейке !изначальный!
-        PState zc1 = cell(U).get_state1();
-
-        // Консервативный вектор в ячейке
-        QState qc1(zc1);
-
-        // Новое значение в ячейке (консервативные переменные)
-        QState Qc = qc1.vec() - m_dt * flux.vec() / cell.volume();
-
-        // Новое значение примитивных переменных
+        // Расчет потока P1
+        Flux P2 = calc_flux(cell, 1);
+        //
+        QState Qc = qc.vec() - m_dt / cell.volume() * P2.vec();
+        //
         PState Zc(Qc, m_eos);
-
-        cell(U).set_state2(Zc);
+        //
+        cell(U).set_state(Zc, 2);
     }
 }
+
+// Рунге-Кутта 4ого порядка
+void SmFluid::fluxes4(Mesh &mesh) {
+    for (auto cell : mesh) {
+        // Примитивный вектор в ячейке
+        PState zc0 = cell(U).get_state(0);
+        // Консервативный вектор в ячейке
+        QState qc0(zc0);
+        // Расчет потока P1
+        cell(U).P1 = calc_flux(cell, 0);
+        //
+        QState Qc1 = qc0.vec() - m_dt / cell.volume() * cell(U).P1.vec();
+        //
+        PState Zc1(Qc1, m_eos);
+        //
+        cell(U).set_state(Zc1, 1);
+    }
+    for (auto cell : mesh) {
+        // Примитивный вектор в ячейке
+        PState zc0 = cell(U).get_state(0);
+        // Консервативный вектор в ячейке
+        QState qc0(zc0);
+        // Расчет потока P2
+        cell(U).P2 = calc_flux(cell, 1);
+        //
+        QState Qc2 = qc0.vec() - 0.5 * m_dt / cell.volume() * cell(U).P1.vec() - m_dt / cell.volume() * cell(U).P2.vec();
+        //
+        PState Zc2(Qc2, m_eos);
+        //
+        cell(U).set_state(Zc2, 2);
+    } 
+    for (auto cell : mesh) {
+        // Примитивный вектор в ячейке
+        PState zc2 = cell(U).get_state(2);
+        // Консервативный вектор в ячейке
+        QState qc2(zc2);
+        // Примитивный вектор в ячейке
+        PState zc1 = cell(U).get_state(1);
+        // Консервативный вектор в ячейке
+        QState qc1(zc1);
+        // Примитивный вектор в ячейке
+        PState zc0 = cell(U).get_state(0);
+        // Консервативный вектор в ячейке
+        QState qc0(zc0);
+        // Расчет потока P3
+        cell(U).P3 = calc_flux(cell, 2);
+        //
+        QState Qc3 = 2 * qc2.vec() - 2 * qc1.vec() + qc0.vec() - 2 * m_dt / cell.volume() * cell(U).P2.vec() + \
+                                                                0.5 * m_dt / cell.volume() * cell(U).P1.vec();
+        //
+        PState Zc3(Qc3, m_eos);
+        //
+        cell(U).set_state(Zc3, 3);
+    }
+    for (auto cell : mesh) {
+        // Примитивный вектор в ячейке
+        PState zc = cell(U).get_state(0);
+        // Консервативный вектор в ячейке
+        QState qc(zc);
+        // Расчет потока P3
+        cell(U).P4 = calc_flux(cell, 3);
+        //
+        QState Qc = qc.vec() - m_dt / 6 / cell.volume() * \
+                    (0.5 * cell(U).P1.vec() + 2 * cell(U).P2.vec() + cell(U).P3.vec() + cell(U).P4.vec());
+        //
+        PState Zc(Qc, m_eos);
+        //
+        cell(U).set_state(Zc, 2);
+    }   
+}
+
+Flux SmFluid::calc_flux(ICell &cell, int stage) {
+    // Примитивный вектор в ячейке
+    PState zc = cell(U).get_state(stage);
+
+    // Консервативный вектор в ячейке
+    QState qc(zc);
+    
+    // Переменная для потока
+    Flux flux;
+    for (auto& face: cell.faces()) {
+        // Внешняя нормаль
+        auto &normal = face.normal();
+
+        // Примитивный вектор соседа
+        PState zn(zc);
+
+        //Единмтаенное различие
+        if (face.is_boundary()) {
+            // Граничные условия типа "стенка"
+            double vn = zc.velocity.dot(face.normal());
+            zn.velocity -= 2.0 * vn * face.normal();
+        } 
+        else {
+            zn = face.neib()(U).get_state(stage);
+        }
+
+        // Значение на грани со стороны ячейки
+        PState zm = zc.in_local(normal);
+
+        // Значение на грани со стороны соседа
+        PState zp = zn.in_local(normal);
+
+        // Численный поток на грани
+        auto loc_flux = m_nf->flux(zm, zp, m_eos);
+        loc_flux.to_global(normal);
+
+        // Суммируем поток
+        flux.vec() += loc_flux.vec() * face.area();
+    }
+
+    return flux;
+};
+
 
 void SmFluid::update(Mesh& mesh) {
     for (auto cell : mesh) {
@@ -247,4 +254,3 @@ void SmFluid::update(Mesh& mesh) {
 }
 }
 }
-
