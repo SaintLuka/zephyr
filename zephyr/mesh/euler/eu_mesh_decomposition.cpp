9ff0b1a4fe363bf37d220d918f14ca67579001df
Kostas <newpontos@gmail.com>
2024-09-19 03:06:30 +0300

    build aliens init

--- zephyr/mesh/euler/eu_mesh_decomposition.cpp
+++ zephyr/mesh/euler/eu_mesh_decomposition.cpp
@@ -74,7 +74,7 @@ void EuMesh::redistribute() {
 
 
     migrate();
-    build_aliens();
+    build_aliens(); // [?] почему после migrate()?
 }
 
 void EuMesh::exchange() {
@@ -86,7 +86,42 @@ void EuMesh::migrate() {
 }
 
 void EuMesh::build_aliens() {
+    int rank = mpi::rank();
 
+    // Выделяем память
+    // [!] думаю, нужно перенести этот код, чтобы он выполнялся единожды
+    m_tourism.m_border_indices.resize(rank);
+    m_tourism.m_count_to_send.resize(rank);
+    m_tourism.m_send_offsets.resize(rank, 0);
+
+    // Заполняем m_border_indices
+    for (auto& cell: m_locals) {
+        for (auto& face: cell.faces) {
+            auto& border_indices = m_tourism.m_border_indices[face.adjacent.rank];
+            if(face.adjacent.rank != rank && (border_indices.empty() || border_indices.back() != cell.index)){
+                // [!?] вместо push_back лучше заранее зарезервировать память m_border_indices и использовать присвоение
+                m_tourism.m_border_indices[face.adjacent.rank].push_back(cell.index);
+            }
+        }
+    }
+
+    // Заполняем m_count_to_send
+    for(int r = 0; r < rank; ++r)
+        m_tourism.m_count_to_send[r] = m_tourism.m_border_indices[r].size();
+    // Заполняем m_send_offsets
+    for(int r = 1; r < rank; ++r)
+        m_tourism.m_send_offsets[r] = m_tourism.m_send_offsets[r - 1] + m_tourism.m_count_to_send[r - 1];
+
+    // Заполняем m_border
+    int border_size = m_tourism.m_send_offsets[rank - 1] + m_tourism.m_count_to_send[rank - 1];
+    m_tourism.m_border.resize(border_size);
+
+    int temp_border_it = 0;
+    for(auto& border_indices : m_tourism.m_border_indices)
+        for(auto& cell_index : border_indices)
+            m_tourism.m_border[temp_border_it++] = m_locals[cell_index];
+
+    // [?] если m_border_indices[r].size() == m_count_to_send[r], то зачем второе вообще нужно?
 }
 
 } // namespace zephyr::mesh
