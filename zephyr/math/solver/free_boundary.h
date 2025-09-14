b06303c214f476528deca2ac194ee419d8533cd2
Alexandra Blanari <aleksandra19052003@gmail.com>
2025-09-14 16:13:29 +0300

    Очищены тест fb_test_1D и решатель FreeBoundary

--- zephyr/math/solver/free_boundary.h
+++ zephyr/math/solver/free_boundary.h
@@ -10,7 +10,6 @@ namespace zephyr::math {
 using zephyr::mesh::EuMesh;
 using zephyr::mesh::EuCell;
 using zephyr::mesh::Storable;
-using zephyr::mesh::Distributor;
 using zephyr::geom::Vector3d;
 using zephyr::phys::Eos;
 
@@ -25,11 +24,7 @@ public:
     /// @brief Расширенный вектор состояния на котором решается задача
     struct Parts {
         Storable<PState> init;  ///< Состояние на основном слое
-        Storable<PState> half;  ///< Состояние на полушаге
         Storable<PState> next;  ///< Состояние на следующем шаге
-
-        /// @brief Градиент вектора состояния
-        Storable<PState> d_dx, d_dy, d_dz;
     };
 
     Parts part;
@@ -46,17 +41,11 @@ public:
     /// @brief Установить число Куранта
     void set_CFL(double CFL);
 
-    /// @brief Задать точность метода (1 или 2)
-    void set_accuracy(int acc);
-
     /// @brief Установить осевую симметрию
     void set_axial(bool axial = true);
 
     /// @brief Установить метод
     void set_method(Fluxes method);
-
-    /// @brief Установить ограничитель градиента
-    void set_limiter(const std::string& limiter);
 
     /// @brief Число Куранта
     double CFL() const;
@@ -69,14 +58,6 @@ public:
 
     /// @brief Выполнить шаг интегрирования по времени
     void update(EuMesh &mesh);
-
-    /// @brief Установить флаги адаптации
-    void set_flags(EuMesh& mesh) const;
-
-    /// @brief Распределитель данных при адаптации
-    /// @param type Тип "const" или "slope" переноса при разбиении
-    Distributor distributor(const std::string& type = "slope") const;
-
 
     /// @brief Посчитать шаг интегрирования по времени с учетом
     /// условия Куранта
@@ -91,18 +72,10 @@ public:
     /// @brief Вычислить производные
     void compute_grad(EuMesh &mesh) const;
 
-    /// @brief Вычислить потоки на стадии предиктора
-    void fluxes_stage1(EuMesh &mesh) const;
-
-    /// @brief Вычислить потоки на стадии корректора
-    void fluxes_stage2(EuMesh &mesh) const;
-
 protected:
     Eos::Ptr m_eos;          ///< Уравнение состояния
-    NumFlux::Ptr m_nf;       ///< Метод расчёта потока
     int m_acc = 1;           ///< Порядок точности
     bool m_axial;            ///< Осевая симметрия
-    Limiter m_limiter;       ///< Ограничитель градиента
     double m_CFL = 0.5;      ///< Число Куранта
     double m_dt;             ///< Шаг интегрирования
     double m_max_dt=1.e300;  ///< Максимальный шаг интегрирования
